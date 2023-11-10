2a011d55240e0f002a86b71440b48da7f31dc964
lrubens <rubensl@stanford.edu>
2023-11-10 15:45:56 -0800

    Updated comal DSE to incorporate block specific latency and initiation interval numbers

--- example_scenarios/synthetic/comal/comal.py
+++ example_scenarios/synthetic/comal/comal.py
@@ -11,31 +11,54 @@ import subprocess
 
 sys.path.append(".")
 from baco.run import optimize  # noqa
+import toml
+from pathlib import Path
+home = str(Path.home())
 
 
 def mha(X):
 
-    x1 = X["par_factor"]
+    par_factor = X["par_factor"]
+    fiberlookup_latency = X["fiberlookup_latency"]
+    fiberlookup_ii = X["fiberlookup_ii"]
 
     rust_binary = "/home/rubensl/Documents/repos/comal/target/release/comal"
     dataset_name = "tensor4_mha"
 
+    with open(home + "/sam_config.toml", "r") as f:
+        config = toml.load(f)
+    
+    config["sam_config"]["fiberlookup_latency"] = fiberlookup_latency
+    config["sam_config"]["fiberlookup_ii"] = fiberlookup_ii
+
+    with open(home + "/sam_config.toml", "w") as f:
+        toml.dump(config, f)
+
     # cmd = rust_binary + " -d " + dataset_name + " --par_factor " + str(x1.astype(np.int32))
-    cmd = [rust_binary, "-d", dataset_name, "--par_factor", str(x1.astype(np.int32))]
+    cmd = [rust_binary, "-d", dataset_name, "--par_factor", str(par_factor.astype(np.int32))]
 
     print(cmd)
     print()
 
     start = time.time() 
 
-    subprocess.run(cmd) 
+    proc = subprocess.run(cmd, universal_newlines = True, stdout = subprocess.PIPE) 
+
+    valid = True
+    cycle_num = 0
+    try:
+        cycle_num = int(proc.stdout.split("\n")[2].split(": ")[1])
+    except:
+        valid = False
+        print("Invalid config")
 
     end = time.time()
-    elapsed_time = end - start
-    print("Time elapsed: ", elapsed_time)
+    # elapsed_time = end - start
+    print("Time elapsed: ", cycle_num)
 
+    # TODO: Add in unknown constraints to output
     return {
-        "compute_time": elapsed_time
+        "compute_time": cycle_num
     }
 
 
