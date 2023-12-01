223b7455489b42fc6c46d8c0a81f5ce4326226bb
lrubens <rubensl@stanford.edu>
2023-11-30 16:48:58 -0800

    Pushing updates to autocal

--- example_scenarios/synthetic/auto_cal/autocal.py
+++ example_scenarios/synthetic/auto_cal/autocal.py
@@ -18,7 +18,7 @@ home = str(Path.home())
 best_diff = 1000000.0
 
 
-def mha(X):
+def autocal(X):
 
     fiberlookup_starting = int(X["fiberlookup_starting"])
     fiberlookup_stop_latency = int(X["fiberlookup_stop_latency"])
@@ -26,21 +26,12 @@ def mha(X):
     fiberlookup_latency = int(X["fiberlookup_latency"])
     fiberlookup_ii = int(X["fiberlookup_ii"])
     incr_type = int(X["incr_type"])
-    # fiberlookup_bump = int(X["bump"])
-    # fiberlookup_stop_bump = int(X["stop_bump"])
-    # fiberlookup_done_bump = int(X["done_bump"])
-    # fiberlookup_empty_bump = int(X["empty_bump"])
-    # fiberlookup_factor = float(X["fiberlookup_factor"])
     fiberlookup_numerator_factor = int(X["fiberlookup_numerator_factor"])
     fiberlookup_denominator_factor = int(X["fiberlookup_denominator_factor"])
     fiberlookup_miss_latency = int(X["fiberlookup_miss_latency"])
 
-    print(X)
-    # print(fiberlookup_ii)
-    # print(fiberlookup_initial)
-    # print(fiberlookup_latency)
-
-    rust_binary = "/home/rubensl/Documents/repos/comal/target/release/deps/comal-eea9c28bcdb18380"
+    # TODO: Change to user input maybe
+    rust_binary = "/home/rubensl/Documents/repos/comal/target/release/deps/comal-c5d2583e53b91909"
 
     with open(home + "/sam_config.toml", "r") as f:
         config = toml.load(f)
@@ -55,11 +46,6 @@ def mha(X):
     config["sam_config"]["fiberlookup_miss_latency"] = fiberlookup_miss_latency
     config["sam_config"]["incr_type"] = incr_type
 
-    # config["sam_config"]["bump"] = fiberlookup_bump
-    # config["sam_config"]["stop_bump"] = fiberlookup_stop_bump
-    # config["sam_config"]["empty_bump"] = fiberlookup_empty_bump
-    # config["sam_config"]["done_bump"] = fiberlookup_done_bump
-
     with open(home + "/sam_config.toml", "w") as f:
         toml.dump(config, f)
 
@@ -67,58 +53,48 @@ def mha(X):
     cmd = [rust_binary, "--nocapture", "rd_scanner",
            "--test-threads=1"]
 
-    print(cmd)
-    print()
-
-    # start = time.time()
-
     proc = subprocess.run(cmd, universal_newlines=True, stdout=subprocess.PIPE)
 
-    print(proc)
-
     diff_lst = []
+
+    # TODO: Hacky way of getting diffs, fix
     for line in proc.stdout.split("\n"):
         if not line.startswith("test templates"):
             continue
-        # print(line)
         item = line.split("... ")[1]
-        # print(item)
-
         diff_lst.append(abs(int(item.split("Diff: ")[1])))
 
-    print(diff_lst)
     avg_diff = 0.0
     for i, diff in enumerate(diff_lst):
-        # if i == 4:
-        #     break
         avg_diff += float(diff)
     avg_diff /= len(diff_lst)
     print("Avg diff: ", avg_diff)
+
     global best_diff
     if avg_diff < best_diff:
         best_diff = avg_diff
     print("Best diff: ", best_diff)
     
 
-    # TODO: Add in unknown constraints to output
+    # TODO: Maybe add in unknown constraints to output
     return {
-        # "diff1": avg_diff,
-        "diff1": float(diff_lst[0]),
-        "diff2": float(diff_lst[1]),
-        "diff3": float(diff_lst[2]),
-        "diff4": float(diff_lst[3]),
-        "diff5": float(diff_lst[4]),
-        "diff6": float(diff_lst[5]),
-        "diff7": float(diff_lst[6]),
-        "diff8": float(diff_lst[7]),
-        "diff9": float(diff_lst[8]),
-        "diff10": float(diff_lst[9]),
+        "diff1": avg_diff,
+        # "diff1": float(diff_lst[0]),
+        # "diff2": float(diff_lst[1]),
+        # "diff3": float(diff_lst[2]),
+        # "diff4": float(diff_lst[3]),
+        # "diff5": float(diff_lst[4]),
+        # "diff6": float(diff_lst[5]),
+        # "diff7": float(diff_lst[6]),
+        # "diff8": float(diff_lst[7]),
+        # "diff9": float(diff_lst[8]),
+        # "diff10": float(diff_lst[9]),
     }
 
 
 def main():
     parameters_file = "example_scenarios/synthetic/auto_cal/autocal.json"
-    optimize(parameters_file, mha)
+    optimize(parameters_file, autocal)
     print("End of known constraints")
 
 
